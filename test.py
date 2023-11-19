f788487f38ecc51c29b835afeb2da6f7ac24f556
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2023-11-19 22:30:42 +0000

    same

--- test.py
+++ test.py
@@ -51,8 +51,14 @@ assert jnp.allclose(jax_axis_length, stel.axis_length, atol=a_tolerance, rtol=r_
 assert jnp.allclose(jax_varphi, stel.varphi, atol=a_tolerance, rtol=r_tolerance)
 assert jnp.allclose(jax_d_d_varphi, stel.d_d_varphi, atol=a_tolerance, rtol=r_tolerance)
 assert jnp.allclose(helicity, stel.helicity, atol=a_tolerance, rtol=r_tolerance)
+import matplotlib.pyplot as plt
+# plt.plot(sigma)
+# plt.plot(stel.sigma)
+# plt.show()
+# exit()
 assert jnp.allclose(elongation, stel.elongation, atol=a_tolerance, rtol=r_tolerance)
 assert jnp.allclose(inv_L_grad_B, stel.inv_L_grad_B, atol=a_tolerance, rtol=r_tolerance)
+assert jnp.allclose(sigma, stel.sigma, atol=a_tolerance, rtol=r_tolerance)
 from qsc.calculate_r1 import _residual, _jacobian
 stel.sigma0 = sigma[0]
 assert jnp.allclose(res, _residual(stel, jnp.concatenate([jnp.array([iota]), sigma[1:]])), atol=a_tolerance, rtol=r_tolerance)
